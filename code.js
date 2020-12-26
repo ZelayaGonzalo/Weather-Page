4e9e03b41cb092e22441328916a30054c65be7a1
ZelayaGonzalo <zelayagonzalo33@gmail.com>
2020-12-26 18:13:44 -0300

    Change background based on weather. Changed fetch to https.

--- code.js
+++ code.js
@@ -1,10 +1,26 @@
+/* eslint-disable default-case */
 const searchValue = document.getElementById('searchBox');
 const form = document.getElementById('form');
 const info = document.getElementById('info');
+const content = document.getElementById('content');
 function kelvinToCelsius(number) {
   return number - 273.15;
 }
+function updateBackgroundImage(weather) {
+  switch (weather) {
+    case 'Clear':
+      content.style.backgroundImage = 'url(Images/clear.jpg)';
+      break;
+    case 'Clouds':
+      content.style.backgroundImage = 'url(Images/cloud.jpg)';
+      break;
+    case 'Rain':
+      content.style.backgroundImage = 'url(Images/rain.jpg)';
+      break;
+  }
+}
 function updateDOM(data) {
+  updateBackgroundImage(data.weather[0].main);
   info.querySelector('.title').innerHTML = `${data.name} , ${data.sys.country}`;
   info.querySelector('.temp').innerHTML = `${kelvinToCelsius(data.main.temp).toFixed(1)} °C`;
   info.querySelector('.feeling').querySelector('#feel').innerHTML = `${kelvinToCelsius(data.main.feels_like).toFixed(1)} °C`;
@@ -13,7 +29,7 @@ function updateDOM(data) {
 }
 async function getTemp(city) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ae3b452c15565eed9225290d1c80cb6e`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ae3b452c15565eed9225290d1c80cb6e`, { mode: 'cors' });
     const temp = await response.json();
     await updateDOM(temp);
     await console.log(temp);
